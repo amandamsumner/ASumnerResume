7dd16ecef5382020088c13adec9f01dec969ba84
Amanda Sumner <amandamsumner@me.com>
2021-05-15 04:34:35 -0400

    added Education Component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,47 @@
 import React, { Component } from "react";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default class Education extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="card">
+          <div className="card-content">
+            <h6>
+              <strong>EDUCATION</strong>
+            </h6>
+            <table className="striped">
+              <thead>
+                <tr>
+                  <th>Certificate</th>
+                  <th>Date</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Full Stack Web Development Certificate</td>
+                  <td>June 2021</td>
+                  <td>
+                    <Link to="#" className="btn blue lighten-2">
+                      View
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Associate of Arts Degree</td>
+                  <td>December 2008</td>
+                  <td>
+                    <Link to="#" className="btn blue lighten-2">
+                      View
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
